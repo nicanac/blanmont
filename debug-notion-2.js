8772502530ad308374bdbeb8ef3eb50c22f98eae
Nicnac <bruyere.nicolas@gmail.com>
2025-12-18 18:19:07 +0100

    feat(trace): refactor doc and add set of instruction for ai

--- debug-notion-2.js
+++
@@ -1,6 +0,0 @@
-const { Client } = require('@notionhq/client');
-
-console.log('Notion Client Structure Test');
-const notion = new Client({ auth: 'secret_test' });
-
-console.log('Keys on notion:', Object.keys(notion));
--- debug-notion-4.js
+++
@@ -1,4 +0,0 @@
-const { Client } = require('@notionhq/client');
-
-const notion = new Client({ auth: 'secret_test' });
-console.log('notion.request exists:', !!notion.request);
